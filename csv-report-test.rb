44917686847666f845684f6964a999812e8c68b8
Ben Nourse <bnourse@gmail.com>
2017-01-27 09:55:59 -0600

    updated with some functions, currently not working

--- csv-report-test.rb
+++ csv-report-test.rb
@@ -1,56 +1,131 @@
 require 'csv'
 require 'pry'
 
-def csvReport
-   accounts = CSV.read('accounts.csv')
-   cleanAccounts = clean(accounts)
-   
-   uniquePayers(cleanAccounts)
+def getAccounts(inputCSV)
+    accounts = []
+    CSV.foreach(inputCSV, {headers: true, return_headers: false}) do |row|
+        accounts.push(row["Account"].strip)
+    end
+    return accounts.uniq
+end
+
+def getCategories(inputCSV)
+    categories = []
+    CSV.foreach(inputCSV, {headers: true, return_headers: false}) do |row|
+        categories.push(row["Category"].strip)
+    end
+    return categories.uniq
+end
+
+def listTransactions(name, category, inputCSV)
+    transactions = []
+    CSV.foreach(inputCSV, {headers: true, return_headers: false}) do |row|
+        strippedName = row["Account"].strip
+        row["Account"] = strippedName
+        strippedCategory = row["Category"].strip
+        row["Category"] = strippedCategory
+        if strippedName == name then
+            if strippedCategory == category then
+                transactions.push(row)
+            end
+        end
+    end
+    return transactions
 end
 
 
-def clean(accounts)
-   cleanAccounts = Array.new
-   
-   for row in accounts
-       cleanRow = Array.new
-       for cell in row
-           #tried with cleanCell = cell.gsub(/\\n/,'')
-           #Strip cleans whitespace before and after
-           #there may be data that it screws up
+def getStartingBalance(name, inputCSV)
+    transactions = []
+    CSV.foreach(inputCSV, {headers: true, return_headers: false}) do |row|
+        strippedName = row["Account"].strip
+        row["Account"] = strippedName
+        strippedPayee = row["Payee"].strip
+        row["Payee"] = strippedPayee
+        if strippedName == name then
+            if strippedPayee == "STARTING BALANCE" then
+                transactions.push(row)
+            end
+        end
+    end
+    return transactions[0]["Inflow"].gsub(/[$]/,'').gsub(/[,]/,'').to_f
+end
 
-           cleanCellDollar = cell.gsub(/[$]/,'')
-           #cleanCell = cleanCellDollar.strip
-           cleanCell = cleanCellDollar.gsub(/\n/,"")
-           cleanRow.push(cleanCell)
-       end
-       cleanAccounts.push(cleanRow)
-   end
-   #puts cleanAccounts.inspect
-   return cleanAccounts
+args = ARGV
+    
+csvFile="accounts.csv"
+
+if args.length > 0 
+    #if we are passed an account name in the command line
+    #only loop over the single account
+    accountsArray = [args[0]]
+else
+    #loop over all the accounts
+    accountsArray = getAccounts(csvFile)
+end
+
+for accountName in accountsArray 
+    #For loop that will loop twice, once for each account name, in this case "Priya" and "Sonia".
+    
+    accountCategories = getCategories(csvFile)
+    totalSpent = 0
+    startingBalance = getStartingBalance(accountName,csvFile)
+    balanceRemaining = 0
+
+    categoryList = []
+    categoryBalanceList = []
+    categoryCountList = []
+    categoryAvgList = []
+
+    for category in accountCategories
+        categoryTransactions = listTransactions(accountName, category, csvFile)
+        categoryTransactionCount = 0
+        categoryBalance = 0
+        categoryList.push(category)
+   
+        for transaction in categoryTransactions
+            #Loops for each transaction for Priya and Sonia. Stripping the "$" and "," then converting to integer.
+            transactionOutflow = transaction[4].gsub(/[$]/,'').gsub(/[,]/,'').to_f
+            transactionInflow = transaction[5].gsub(/[$]/,'').gsub(/[,]/,'').to_f
+            categoryTransactionCount = categoryTransactionCount + 1
+            categoryBalance = categoryBalance  - transactionOutflow + transactionInflow
+        end
+
+        categoryBalanceList.push(categoryBalance.round(2))
+        categoryCountList.push(categoryTransactionCount)
+
+        if categoryTransactionCount > 0 
+            categoryAvg = categoryBalance / categoryTransactionCount
+        else
+            categoryAvg = 0
+        end
+
+        categoryAvgList.push(categoryAvg)
+
+
+    end
+
+    for categoryBalance in categoryBalanceList
+        balanceRemaining = balanceRemaining + categoryBalance
+    end
+
+
+    #console output stuff goes here
+    #remember to skip outputting a category if categoryCountList[i] == 0
+    print ("=" * 80) + "\n"
+    print "Account: " + accountName + "... Balance: $" + balanceRemaining.round(2).to_s + "\n"
+    print ("=" * 80) + "\n"
+    print "Category" + (" " * 22) + "|  " + "Total Spent" + (" " * 4) + "|  " + "Average Transaction" + "\n"
+    print ("-" * 29) + " | " + ("-" * 15) + " | " + ("-" * 30) + "\n"
+    for i in 0..categoryList.length-1
+        
+        if categoryCountList[i] > 0 then
+            print categoryList[i] + (" " * (30 - categoryList[i].length)) + "|  " + categoryBalanceList[i].round(2).to_s + (" " * (15 - categoryBalanceList[i].to_s.length)) + "|  " + categoryAvgList[i].round(2).to_s + "\n" 
+        end 
+
+    end
+    print "\n" + "\n"
+
 end
 
 
-def uniquePayers(allOneAccount)
 
-   kvpByName = {}
-   
-   for i in 1..allOneAccount.length - 1
-
-       entrySet = allOneAccount[i]
-       id = entrySet[0]
-
-       unless kvpByName.has_key?(id)
-           kvpByName[id] = []
-
-       end
-
-       kvpByName[id].push(entrySet)
-
-
-   end
-   puts kvpByName
-
-end
-
-csvReport
